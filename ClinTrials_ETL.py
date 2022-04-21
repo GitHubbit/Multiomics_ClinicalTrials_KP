835235398f4321280cef6eaf832849a0d1b2dde3
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2022-04-21 12:56:05 -0700

    Changed from Jupyter notebook to regular python script

---
+++ ClinTrials_ETL.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+# shell command: brew install postgresql 
+import pandas as pd
+import psycopg2
+import pandas.io.sql as sqlio
+
+
+# connect to DB and get the column names of the table
+con = None
+con = psycopg2.connect(database="aact")
+con.rollback()
+cursor = con.cursor()
+
+con.autocommit = True # SQL statement is treated as a transaction and is automatically committed right after it is executed
+sql = '''SELECT * FROM ctgov.conditions;'''
+cursor.execute(sql)
+column_names = [desc[0] for desc in cursor.description]
+tuples = cursor.fetchall()
+con.close()
+df = pd.DataFrame(tuples, columns=column_names)
