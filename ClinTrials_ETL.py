8590ce630af3686fb79e4dafff268e8de9f7232b
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2022-06-29 10:21:15 -0700

    Testing on larger subset of data

--- ClinTrials_ETL.py
+++ ClinTrials_ETL.py
@@ -303,8 +303,8 @@ def driver():
     # conditions_proc = dict(random.sample(ct_processed.get("conditions").items(), 2000))   # deprecated in Python 3.9+
     # interventions_proc = dict(random.sample(ct_processed.get("interventions").items(), 2000)) # deprecated in Python 3.9+
 
-    conditions_proc = dict(random.sample(list(ct_processed.get("conditions").items()), k=50))
-    interventions_proc = dict(random.sample(list(ct_processed.get("interventions").items()), k=50))
+    conditions_proc = dict(random.sample(list(ct_processed.get("conditions").items()), k=2000))
+    interventions_proc = dict(random.sample(list(ct_processed.get("interventions").items()), k=2000))
 
     # print(conditions_proc)
 
