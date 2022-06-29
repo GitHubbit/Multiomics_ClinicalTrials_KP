020e47a4b4ffdd5636ceffb89ba4ac33e1895562
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2022-06-28 19:56:43 -0700

    ClinTrials_ETL.py script now updated to match ClinicalTrials_playground.ipynb. This will output baseline version of nodes and edges files for KG. First edition

--- ClinTrials_ETL.py
+++ ClinTrials_ETL.py
@@ -12,6 +12,8 @@ import sys
 import multiprocessing
 import pickle
 import json
+from itertools import repeat
+
 
 # adding Folder_2/subfolder to the system path
 sys.path.insert(0, '/Volumes/TOSHIBA_EXT/ISB/clinical_trials/pymetamap-master')
@@ -222,7 +224,6 @@ def stop_metamap_servers():
     os.system(metamap_base_dir + metamap_pos_server_dir + ' stop') # Part of speech tagger
     os.system(metamap_base_dir + metamap_wsd_server_dir + ' stop') # Word sense disambiguation 
 
-
 def run_metamap(input_term, source_restriction):
     mm = MetaMap.get_instance(metamap_base_dir + metamap_bin_dir)
     concepts_dict = dict()
@@ -273,6 +274,17 @@ def write_output_out(output_to_store):
     #     file.write(json.dumps(output_to_store, indent=4))
 
 
+def get_mapped_ct_data(metamapped_concept, translation_dict):
+    for key, val in metamapped_concept.items():
+        for concept in val:
+            concept = concept._asdict()
+            row = [key]
+            original_concept = (list(translation_dict.keys())[list(translation_dict.values()).index(key)])
+            row.append(original_concept)
+            row.extend([concept.get(k) for k in ['preferred_name', 'cui', 'score', 'semtypes']])
+            return row # we only want the first mapped concept
+
+
 
 def driver():
     # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
@@ -283,12 +295,12 @@ def driver():
     ct_preprocessed = preprocess_ct_data(ct_data)
     ct_processed = select_cols_to_preprocess(ct_preprocessed)
     # ct_processed.get("conditions")
-    # ct_processed.get("interventions")
+    # print(ct_processed.get("interventions"))
     start_metamap_servers()
     
     import random
-    conditions_proc = dict(random.sample(ct_processed.get("conditions").items(), 20))
-    interventions_proc = dict(random.sample(ct_processed.get("conditions").items(), 20))
+    conditions_proc = dict(random.sample(ct_processed.get("conditions").items(), 2000))
+    interventions_proc = dict(random.sample(ct_processed.get("interventions").items(), 2000))
 
     # print(conditions_proc.values())
 
@@ -301,16 +313,6 @@ def driver():
     metamapped_interventions = multiprocessing.Pool(multiprocessing.cpu_count() - 1).starmap(run_metamap, 
                                                                              zip(list(interventions_proc.values()),
                                                                                  [[None]]*len(list(interventions_proc.values()))))
-    stop_metamap_servers()
-    metamapped_dict["conditions"] = metamapped_conditions
-    metamapped_dict["interventions"] = metamapped_interventions
-
-    # print(metamapped_dict.get("conditions"))
-
-
-
-
-
 
     # UNCOMMENT TO RUN ON FULL DATASET
     # metamapped_conditions = multiprocessing.Pool(multiprocessing.cpu_count() - 1).starmap(run_metamap,
@@ -321,8 +323,107 @@ def driver():
     #     zip(list(ct_processed.get("interventions").values()),
     #         [['SNOMEDCT_US', 'SNOMEDCT_VET', 'ICD10CM', 'ICD10CM', 'ICD10PCS', 'ICD9CM', 'ICD9CM']]*len(list(ct_processed.get("interventions").values()))))
 
-    
+    stop_metamap_servers()
+    metamapped_dict["conditions"] = metamapped_conditions
+    metamapped_dict["interventions"] = metamapped_interventions
     write_output_out(metamapped_dict)
+
+
+    # with open('metamapped_conditions.txt', 'rb') as f:
+    #     metamapped_conditions = pickle.load(f)
+    
+    # with open('metamapped_interventions.txt', 'rb') as f:
+    #     metamapped_interventions = pickle.load(f)
+
+
+    # print(metamapped_dict.get("conditions"))
+    # print("\n\n\n\n")
+    # print(ct_processed.get("conditions"))
+    
+    with multiprocessing.Pool(multiprocessing.cpu_count() - 1) as pool:
+        final_metamapped_conditions = pool.starmap(get_mapped_ct_data,
+            zip(metamapped_dict.get("conditions"),
+                repeat(ct_processed.get("conditions"))))
+        final_metamapped_interventions = pool.starmap(get_mapped_ct_data,
+            zip(metamapped_dict.get("interventions"),
+                repeat(ct_processed.get("interventions"))))
+
+    master_condition_dict = {condition[0]:condition[1:] for condition in final_metamapped_conditions if condition}  
+    master_intervention_dict = {intervention[0]:intervention[1:] for intervention in final_metamapped_interventions if intervention}    
+
+    baseline_final_condition_dict = {}
+    baseline_final_intervention_dict = {}
+
+    for k, v in master_condition_dict.items():
+        orig_con = k.lower()
+        orig_con = orig_con.strip()
+        mapped_con = str(v[1])
+        mapped_con = mapped_con.lower()
+        mapped_con = mapped_con.strip()
+        if orig_con == mapped_con:
+            baseline_final_condition_dict[k] = v[1:] 
+            
+    for k, v in master_intervention_dict.items():
+        orig_int = k.lower()
+        orig_int = orig_int.strip()
+        mapped_int = str(v[1])
+        mapped_int = mapped_int.lower()
+        mapped_int = mapped_int.strip()
+        if orig_int == mapped_int:
+            baseline_final_intervention_dict[k] = v[1:] 
+
+    # create CURIEs from values of baseline lists
+    condition_curie_dict = {}
+    intervention_curie_dict = {}
+
+    for k, v in baseline_final_condition_dict.items():
+        condition_curie_dict[k] = "UMLS:{}".format(v[1])    
+    for k, v in baseline_final_intervention_dict.items():
+        intervention_curie_dict[k] = "UMLS:{}".format(v[1])
+
+    # print(intervention_curie_dict)
+
+    ct_final_mapped = ct_preprocessed.copy()
+    print(ct_final_mapped[:10])
+    ct_final_mapped['subject'] = ct_preprocessed['subject_name'].map(condition_curie_dict)
+    ct_final_mapped['object'] = ct_preprocessed['object_name'].map(intervention_curie_dict)
+
+    ct_final_mapped = ct_final_mapped.dropna(subset=['subject','object'])
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(ct_final_mapped.head(20))    
+    
+    # make dataframe for nodes file derived from edges dataframe (ct_final_mapped)
+    ct_nodes_subjects = ct_final_mapped[['subject', 'subject_name']]
+    ct_nodes_subjects.columns = ['id', 'name']
+    ct_nodes_subjects = ct_nodes_subjects.assign(category='biolink:DiseaseOrPhenotypicFeature')
+
+    ct_nodes_objects = ct_final_mapped[['object', 'object_name']]
+    ct_nodes_objects.columns = ['id', 'name']
+    ct_nodes_objects = ct_nodes_objects.assign(category='biolink:Treatment')
+
+    ct_nodes = pd.concat([ct_nodes_subjects, ct_nodes_objects], ignore_index=True, axis=0)
+    ct_nodes = ct_nodes.drop_duplicates('id')
+
+    # write nodes and edges files
+
+    ct_final_mapped.to_csv('ClinTrials_KG_edges_v01.csv', sep ='\t', index=False)
+    ct_nodes.to_csv('ClinTrials_KG_nodes_v01.csv', sep ='\t', index=False)
+
+
+
+    # print(baseline_final_intervention_dict)
+
+    # print(master_intervention_dict) 
+    # master_disease_dict = {disease[0]:disease[1:] for disease in final_metamapped_diseases if disease}  
+    # master_intervention_dict = {intervention[0]:intervention[1:] for intervention in final_metamapped_interventions if intervention}    
+
+
+
+
+
+    
+
+    
 
     # print(metamapped_conditions)
 
