9914839eb59d5a2dd5ae744c94dd46b32ae89193
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2022-07-13 17:34:12 -0700

    Added functionality that processes both .zip files and _extracted product of extracting a zip file

--- version.py
+++ version.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env conda run -n ct_extract_env python
-import ClinTrials_ETL
 
-# def get_release(version_date):
-# 	return ClinTrials_ETL.version_date
-
-# get_release()
 
 def get_release(self):
-    # hard-coded release for demo purpose
-    # "self" is a dumper instance, see:
-    # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
-    return "1.0"
+	
+	# below commented block works for flat files (that have been downloaded and extracted)
+	# dir_list = [folds for folds in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir,folds))]
+	# dir_list = [i.split("_")[0] for i in dir_list if "_extracted" in i]
+	# file_dates = [dt.datetime.strptime(date, '%Y%m%d').date() for date in test_dir_list] # convert all strings in list into datetime objects
+	# latest_file_date = max(file_dates)
+	# latest_file_date = latest_file_date.strftime('%m/%d/%Y')
+	# return latest_file_date
+
+	
+
+	# below works for zip files
+	def get_dates(date):
+	    try:
+	        return dt.datetime.strptime(date, '%Y%m%d').date()
+	    except ValueError:
+	        pass
+  
+
+	file_list = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f)) and  f.endswith(".zip")]
+	file_list = [i.split("_")[0] for i in dir_list]
+	file_list = [get_dates(x) for x in file_list]
+	file_list = list(filter(None, file_list))
+
+	latest_file_date = max(file_list)
+	latest_file_date = latest_file_date.strftime('%m/%d/%Y')
+	return latest_file_date
+
+
+ 
