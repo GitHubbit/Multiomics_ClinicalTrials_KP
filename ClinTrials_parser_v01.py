67e9f77ffa7ccb2da41b4416ce28c383203b200f
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2022-08-09 15:48:17 -0700

    Just changed input edges file for reading in data

--- ClinTrials_parser_v01.py
+++ ClinTrials_parser_v01.py
@@ -7,7 +7,7 @@ import json
 
 def parse_edges(data_folder):
     
-    edges = pd.read_csv("ClinTrials_KG_edges_v01_2.csv", sep='\t')
+    edges = pd.read_csv("ClinTrials_KG_edges_v01_3.csv", sep='\t')
 
     for index, row in edges.iterrows():
         id_dict = {}
