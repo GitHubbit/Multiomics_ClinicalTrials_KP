8fb9756f5fc4114ca28889a6259a84f0c324ad99
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2024-02-29 08:43:17 -0800

    Raising max_workers to 100. Processing term_list_pairs in batches of 100. Garbage collecting every 50 terms submitted

--- ClinTrials_ETL.py
+++ ClinTrials_ETL.py
@@ -471,7 +471,7 @@ def parallelize_mappers(term_pair_list, params, term_type, csv_writer):
     start_metamap_servers(metamap_dirs) # start the MetaMap servers
     terms_left = len(term_pair_list)
     future_to_pair = {}
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         future_to_pair = {executor.submit(run_mappers, term_pair, params, term_type, csv_writer): term_pair for term_pair in term_pair_list}
         try:
             for future in concurrent.futures.as_completed(future_to_pair, timeout=120): # timeout after 2 min
@@ -483,7 +483,7 @@ def parallelize_mappers(term_pair_list, params, term_type, csv_writer):
                     print(f"Job {term_pair} generated an exception: {exc}")
                 finally:
                     terms_left -= 1
-                    if terms_left % 10 == 0:
+                    if terms_left % 50 == 0:
                         gc.collect()
                         time.sleep(2)
         except concurrent.futures.TimeoutError:
@@ -528,7 +528,7 @@ def term_list_to_mappers(dict_new_terms):
     intervention_alts_params = intervention_params # same params as interventions
     # intervention_alternate_term_type = "intervention_alternate"
     
-    chunksize = 50
+    chunksize = 100
     
     if metamap_version[0] >= 20:
         
