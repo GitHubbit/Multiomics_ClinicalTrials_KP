a4482d7a35e17a963b8246e15ae3ebc75b697d1d
GitHubbit <10294502+GitHubbit@users.noreply.github.com>
2024-03-05 12:01:12 -0800

    trivial

--- ClinTrials_ETL.py
+++ ClinTrials_ETL.py
@@ -426,7 +426,7 @@ def run_mappers(term_pair, params, term_type, mapping_filename):
 
         for i in dummy_list:
             input_term = i
-            concepts,error = mm.extract_concepts([input_term], restrict_to_sts = ['acab,anab,cgab,comd,dsyn,inpo,mobd,neop,patf,clna,fndg'])
+            concepts,error = mm.extract_concepts([input_term])
             print(concepts)
 
         # mm = MetaMap.get_instance(metamap_dirs["metamap_base_dir"] + metamap_dirs["metamap_bin_dir"])
